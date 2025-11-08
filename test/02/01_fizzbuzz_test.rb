849c7e7ce21369e0f7ce1b509e0bd66446e99326
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-08 03:43:44 +0000

    Add test

--- test/02/01_fizzbuzz_test.rb
+++ test/02/01_fizzbuzz_test.rb
@@ -4,4 +4,8 @@ class FizzBuzzTest < Minitest::Test
   def test_returns_number_for_one
     assert_equal '1', fizzbuzz(1)
   end
+
+  def test_returns_number_for_two
+    assert_equal '2', fizzbuzz(2)
+  end
 end
