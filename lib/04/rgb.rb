7ddaeb4e972f2b0e988d332c480cc00c4cf4dd37
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-10 11:18:50 +0000

    Update test & imple

--- lib/04/rgb.rb
+++ lib/04/rgb.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 def to_hex(r, g, b)
-  '#000000'
+  hex_r = r.to_s(16).rjust(2, '0')
+  hex_g = g.to_s(16).rjust(2, '0')
+  hex_b = b.to_s(16).rjust(2, '0')
+
+  "##{hex_r}#{hex_g}#{hex_b}"
 end
