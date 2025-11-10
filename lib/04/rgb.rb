f98d8ff60e5abb0814f9a70c1ef4139edd3f5908
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-10 11:22:20 +0000

    Refactor again

--- lib/04/rgb.rb
+++ lib/04/rgb.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
 def to_hex(r, g, b)
-  hex = '#'
-  [r, g, b].each do |n|
-    hex += n.to_s(16).rjust(2, '0')
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2, '0')
   end
-
-  hex
 end
