fd628dab791b103b0db80fcbf81f8a533f283c70
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-10 11:10:13 +0000

    Create first test (fail)

---
+++ test/04/rgb_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+# require_relative '../../lib/04/rgb'
+
+class RgbTest < Minitest::Test
+  def test_to_hex
+    assert_equal '#000000', to_hex(0, 0, 0)
+  end
+end
