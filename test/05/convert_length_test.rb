e9a87aaabe7a24fca4c0a9e5c087252cf899216e
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-11 13:00:50 +0000

    Update test & imple

--- test/05/convert_length_test.rb
+++ test/05/convert_length_test.rb
@@ -6,5 +6,7 @@ require_relative '../../lib/05/convert_length'
 class ConvertLengthTest < Minitest::Test
   def test_m_to_in
     assert_equal 39.37, convert_length(1, 'm', 'in')
+    assert_equal 0.38, convert_length(15, 'in', 'm')
+    assert_equal 10_670.73, convert_length(35_000, 'ft', 'm')
   end
 end
