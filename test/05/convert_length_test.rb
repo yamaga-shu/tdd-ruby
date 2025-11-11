7117f0e1e550bdb403fe1c46c60b64b5634becaa
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-11 13:07:54 +0000

    Refactor imple
    convert argument to symbol from string

--- test/05/convert_length_test.rb
+++ test/05/convert_length_test.rb
@@ -5,8 +5,8 @@ require_relative '../../lib/05/convert_length'
 
 class ConvertLengthTest < Minitest::Test
   def test_m_to_in
-    assert_equal 39.37, convert_length(1, :m, :in)
-    assert_equal 0.38, convert_length(15, :in, :m)
-    assert_equal 10_670.73, convert_length(35_000, :ft, :m)
+    assert_equal 39.37, convert_length(1, from: :m, to: :in)
+    assert_equal 0.38, convert_length(15, from: :in, to: :m)
+    assert_equal 10_670.73, convert_length(35_000, from: :ft, to: :m)
   end
 end
