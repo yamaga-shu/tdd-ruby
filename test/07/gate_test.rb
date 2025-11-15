aeafd941ff23142e21e8173db894fed2d94f93a4
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-15 06:53:13 +0000

    Add test case
    not enough fare

--- test/07/gate_test.rb
+++ test/07/gate_test.rb
@@ -14,4 +14,14 @@ class GateTest < Minitest::Test
     umeda.enter(ticket)
     assert juso.exit(ticket)
   end
+
+  def test_umeda_to_mikuni_not_enough_fare
+    umeda = Gate.new(:umeda)
+    mikuni = Gate.new(:mikuni)
+
+    ticket = Ticket.new(160)
+
+    umeda.enter(ticket)
+    refute mikuni.exit(ticket)
+  end
 end
