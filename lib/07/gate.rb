5581111cf10487bebceb32267bc9326491a44033
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-15 07:17:19 +0000

    Update imple to pass test

--- lib/07/gate.rb
+++ lib/07/gate.rb
@@ -3,14 +3,32 @@
 # Gate represents a Station's Gate
 # which checks if it is possible to go through with a Ticket.
 class Gate
+  STATIONS = %i[umeda juso mikuni].freeze
+  FARES = [160, 190].freeze
+
   def initialize(name)
     @name = name
   end
 
   def enter(ticket)
+    ticket.stamp(@name)
   end
 
   def exit(ticket)
-    true
+    fare = calc_fare(ticket)
+    fare <= ticket.fare
+  end
+
+  private
+
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+
+    raise 'Invalid station' if from.nil? || to.nil?
+
+    distance = to - from
+
+    FARES[distance - 1]
   end
 end
--- lib/07/ticket.rb
+++ lib/07/ticket.rb
@@ -3,7 +3,13 @@
 # Ticket represents Train's Ticket
 # which has its fare and would have stamped station name where entered.
 class Ticket
+  attr_reader :fare, :stamped_at
+
   def initialize(fare)
     @fare = fare
+  end
+
+  def stamp(name)
+    @stamped_at = name
   end
 end
