e9a089cd17eb6e0eb149fdb2b22b3ac314fac669
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-15 07:22:20 +0000

    Add test case

--- test/07/gate_test.rb
+++ test/07/gate_test.rb
@@ -5,23 +5,37 @@ require_relative '../../lib/07/gate'
 require_relative '../../lib/07/ticket'
 
 class GateTest < Minitest::Test
-  def test_gate_umeda_to_juso
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
 
+  def test_gate_umeda_to_juso
     ticket = Ticket.new(160)
 
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
+  end
+
+  def test_juso_to_mikuni
+    ticket = Ticket.new(160)
+
+    @juso.enter(ticket)
+    assert @mikuni.exit(ticket)
+  end
+
+  def test_umeda_to_mikuni
+    ticket = Ticket.new(190)
+
+    @umeda.enter(ticket)
+    assert @mikuni.exit(ticket)
   end
 
   def test_umeda_to_mikuni_not_enough_fare
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
-
     ticket = Ticket.new(160)
 
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
   end
 end
