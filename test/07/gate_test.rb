6d61dd2738c26834ec07d6669ed65851933c5d28
shu.yamaga <s.yamaga.0318@gmail.com>
2025-11-15 06:22:26 +0000

    Update test
    create Umeda & Juso Gate.
    And passed with Ticket.

--- test/07/gate_test.rb
+++ test/07/gate_test.rb
@@ -4,7 +4,13 @@ require_relative '../test_helper'
 require_relative '../../lib/07/gate'
 
 class GateTest < Minitest::Test
-  def test_gate
-    assert Gate.new
+  def test_gate_umeda_to_juso
+    umeda = Gate.new(:umeda)
+    juso = Gate.new(:juso)
+
+    ticket = Ticket.new(160)
+
+    umeda.enter(ticket)
+    assert juso.exit(ticket)
   end
 end
